0fa0e56816f40f9a3f3f94ae208088326eae6da4
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-26 22:13:28 +0300

    pkg was git ignored

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -6,17 +6,14 @@ import (
 )
 
 type Day struct {
-	Date string
+	Date      string
 	DayOfWeek string
-	Info string
+	Info      string
 }
 
 type Week []Day
 
 var daysOfWeek = []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"}
-
-// GenerateMockData generates mock data for the given date and number of weeks
-
 
 func GenerateMockData(startingDate string, numWeeks int) []Week {
 	formattedDate, err := time.Parse("2006-01-02", startingDate)
@@ -29,9 +26,9 @@ func GenerateMockData(startingDate string, numWeeks int) []Week {
 		for d := 0; d < 7; d++ {
 			date := formattedDate.Add(time.Duration(w*7+d) * 24 * time.Hour)
 			week = append(week, Day{
-				Date: date.Format("02 Jan"),
-				DayOfWeek: daysOfWeek[d % 5],
-				Info: "info for" + date.Format("02 Jan"),
+				Date:      date.Format("02 Jan"),
+				DayOfWeek: daysOfWeek[d%5],
+				Info:      "info for" + date.Format("02 Jan"),
 			})
 		}
 		weeks = append(weeks, week)
