04d413810ab3fe7e6b82efb2c095de016518abe4
Mohamed Faris <mohd.faris07@gmail.com>
2024-03-08 21:42:58 +0300

    no more echo

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,13 @@ import (
 	"io"
 	"log"
 	"net/http"
-
-	"github.com/iSolate77/htmx-calendar/pkg/api"
-	"github.com/labstack/echo/v4"
 )
 
 type Templatereader struct {
 	templates *template.Template
 }
 
-func (t *Templatereader) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+func (t *Templatereader) Render(w io.Writer, name string, data interface{}) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
@@ -24,35 +21,20 @@ func main() {
 		log.Fatalf("Error parsing templates: %s", err.Error())
 	}
 
-	e := echo.New()
-	e.Static("/static", "web/static")
-
-	e.Renderer = &Templatereader{
+	renderer := &Templatereader{
 		templates: tmpls,
 	}
 
-	e.GET("/", func(c echo.Context) error {
-		mockData := api.GenerateMockData("2020-01-01", 16)
-		err = c.Render(http.StatusOK, "base.html", map[string]interface{}{
-			"Weeks": mockData,
-		})
-		if err != nil {
-			log.Printf("Error rendering template: %s", err.Error())
-			return c.String(http.StatusInternalServerError, "Error rendering template")
-		}
-		return nil
+	mux := http.NewServeMux()
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		renderer.Render(w, "base.html", nil)
 	})
 
-	e.GET("/toggle-display-endpoint", menuDropdown)
-
-	e.GET("/status", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Test response")
+	mux.HandleFunc("GET /toggle-display-endpoint", func(w http.ResponseWriter, r *http.Request) {
+		renderer.Render(w, "dropdownMenu.html", nil)
 	})
 
 	log.Println("Server started on port 8080")
-	e.Start(":8080")
-}
-
-func menuDropdown(c echo.Context) error {
-	return c.Render(http.StatusOK, "dropdownMenu.html", nil)
+	http.ListenAndServe(":8080", mux)
 }
